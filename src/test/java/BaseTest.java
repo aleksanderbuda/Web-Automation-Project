39afdc13478321a78a8104bffc428913c0078e01
= <viciousdelicious02@gmail.com>
2024-04-02 13:22:58 +0200

    para bank jenkins commit #2

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,3 +1,5 @@
+import config.ConfigLoader;
+import config.DockerSetup;
 import config.Drivers;
 import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
@@ -16,6 +18,16 @@ public class BaseTest {
     protected static Logger log = LogManager.getLogger();
     protected static String browserType;
 
+//    @BeforeSuite
+//    public void startDocker() throws IOException, InterruptedException {
+//        ConfigLoader configLoader = new ConfigLoader("src/main/java/config/configuration.properties");
+//        boolean isRemote = configLoader.isRemote();
+//        if (isRemote) {
+//            log.info("Starting Selenium Grid on Docker container");
+//            DockerSetup.startDockerGrid();
+//        }
+//    }
+
     @BeforeClass
     public void adminPageSettings() throws InterruptedException, MalformedURLException {
         this.driver = Drivers.getDriver(browserType);
@@ -23,31 +35,45 @@ public class BaseTest {
         adminPage.adminSettingsSetUp();
         closeBrowser();
     }
+
     @BeforeMethod
     public void setup() throws MalformedURLException {
         this.driver = Drivers.getDriver(browserType);
     }
+
     @AfterMethod
     public void teardown() {
         driver.quit();
     }
+
     @AfterClass
     public void closeBrowser() {
         if (driver != null) {
             driver.quit();
         }
     }
+
+//    @AfterSuite
+//    public void stopDocker() throws IOException, InterruptedException {
+//        ConfigLoader configLoader = new ConfigLoader("src/main/java/config/configuration.properties");
+//        boolean isRemote = configLoader.isRemote();
+//        if (isRemote) {
+//            log.info("Stopping Selenium Grid on Docker container");
+//            DockerSetup.stopDockerGrid();
+//        }
+//    }
     public void failedTest(String testMethodName, String screenshotDirectory, WebDriver driver) {
         File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         try {
             String screenshotPath = screenshotDirectory + "\\" + testMethodName + "_" + System.currentTimeMillis() + ".png";
             FileUtils.copyFile(screenshotFile, new File(screenshotPath));
         } catch (IOException e) {
-           log.error("Error saving screenshot: " + e.getMessage());
+            log.error("Error saving screenshot: " + e.getMessage());
 
         }
     }
 }
+
 
 
 
