2d8d66f91bf9fb06dc3d7fd526ea243329bbb21a
= <viciousdelicious02@gmail.com>
2024-04-02 13:34:12 +0200

    para bank jenkins commit #3

--- src/main/java/config/DockerSetup.java
+++ src/main/java/config/DockerSetup.java
@@ -6,22 +6,16 @@ import java.io.IOException;
 public class DockerSetup {
 
     public static void startDockerGrid() throws IOException, InterruptedException {
-        String projectDir = new File("C:\\Users\\budaa\\Desktop\\project java\\Para_Bank_Framework\\docker").getAbsolutePath();
-        String startDockerPath = projectDir + "\\start-docker.bat";  // Adjust the path separator for Windows
-        String[] startDocker = { "cmd", "/c", "start", startDockerPath };
+        String[] startDocker = { "cmd", "/c", "start", "docker\\start_docker.bat" };
         ProcessBuilder processBuilder = new ProcessBuilder(startDocker);
-        processBuilder.directory(new File(projectDir)); // Set the working directory
         processBuilder.start();
         Thread.sleep(15000);
     }
 
     public static void stopDockerGrid() throws IOException, InterruptedException {
-        String projectDir = new File("").getAbsolutePath();
-        String stopDockerPath = projectDir + "\\docker\\stop-docker.bat"; // Adjust the path separator for Windows
-        String[] stopDocker = { "cmd", "/c", "start", stopDockerPath };
+        String[] stopDocker = { "cmd", "/c", "start", "docker\\stop_docker.bat" };
         String[] killTask = { "taskkill", "/f", "/im", "cmd.exe" };
         ProcessBuilder processBuilder1 = new ProcessBuilder(stopDocker);
-        processBuilder1.directory(new File(projectDir)); // Set the working directory
         processBuilder1.start();
         Thread.sleep(6000);
 
--- src/main/java/config/Drivers.java
+++ src/main/java/config/Drivers.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 import java.util.Properties;
 
 public class Drivers {
@@ -71,6 +72,7 @@ public class Drivers {
                 }
             }
         }
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 //        driver.manage().window().maximize();
         return driver;
     }
