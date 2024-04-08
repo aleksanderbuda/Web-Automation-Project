1bd9654300ac08d16618304e149ab78ca81215ac
= <viciousdelicious02@gmail.com>
2024-04-08 13:29:39 +0200

    Allure update

--- src/test/java/ParaBankTests.java
+++ src/test/java/ParaBankTests.java
@@ -1,23 +1,38 @@
-import config.Drivers;
 import org.testng.Assert;
-import static org.testng.Reporter.getCurrentTestResult;
-
-import org.testng.ITestResult;
 import org.testng.annotations.Test;
 import config.Log;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Step;
+
+
+import static org.testng.Reporter.getCurrentTestResult;
 
 public class ParaBankTests extends BaseTest {
 
-    @Test(priority = 1)
+    @Epic("ParaBank Account Management")
+    @Feature("New Account Creation")
+    @Test(priority = 1, description = "Validate registration form fields")
     public void registrationFieldValidationTest() throws InterruptedException {
+        registrationFieldValidationSteps();
+    }
+
+    @Step("Performing registration field validation")
+    public void registrationFieldValidationSteps() throws InterruptedException {
         String testCaseName = getCurrentTestName(getCurrentTestResult());
         Log.startTestCase(testCaseName);
         UserSignupFormValidation registrationFieldValidation = new UserSignupFormValidation(driver);
         registrationFieldValidation.validateSignupForm();
         Log.endTestCase(testCaseName);
     }
-    @Test(priority = 2)
+
+    @Test(priority = 2, description = "Login to ParaBank with valid credentials")
     public void userLogInTest() throws InterruptedException {
+        userLogInSteps();
+    }
+
+    @Step("Performing user login")
+    public void userLogInSteps() throws InterruptedException {
         String testCaseName = getCurrentTestName(getCurrentTestResult());
         Log.startTestCase(testCaseName);
         UserLoginService paraBankLogIn = new UserLoginService(driver);
@@ -28,8 +43,14 @@ public class ParaBankTests extends BaseTest {
         log.info("User login successful");
         Log.endTestCase(testCaseName);
     }
-    @Test(priority = 3)
+
+    @Test(priority = 3, description = "Open a new bank account")
     public void userOpensNewBankAccountTest() throws InterruptedException {
+        userOpensNewBankAccountSteps();
+    }
+
+    @Step("Opening a new bank account")
+    public void userOpensNewBankAccountSteps() throws InterruptedException {
         String testCaseName = getCurrentTestName(getCurrentTestResult());
         Log.startTestCase(testCaseName);
         NewBankAccountOpener openNewBankAccount = new NewBankAccountOpener(driver);
@@ -45,8 +66,14 @@ public class ParaBankTests extends BaseTest {
         Assert.assertEquals(openNewBankAccount.getSecondBalanceText(), expectedSecondBalance, "Incorrect second account balance");
         Log.endTestCase(testCaseName);
     }
-    @Test(priority = 4)
+
+    @Test(priority = 4, description = "Transfer funds between accounts")
     public void userTransfersTheFundsTest() throws InterruptedException {
+        userTransfersTheFundsSteps();
+    }
+
+    @Step("Transferring funds between accounts")
+    public void userTransfersTheFundsSteps() throws InterruptedException {
         String testCaseName = getCurrentTestName(getCurrentTestResult());
         Log.startTestCase(testCaseName);
         NewBankAccountOpener openNewBankAccount = new NewBankAccountOpener(driver);
@@ -61,8 +88,14 @@ public class ParaBankTests extends BaseTest {
         Assert.assertTrue(transferFunds.isBalanceCorrectForSecondRow());
         Log.endTestCase(testCaseName);
     }
-    @Test(priority = 5)
+
+    @Test(priority = 5, description = "Request a loan")
     public void userRequestsLoanTest() throws InterruptedException {
+        userRequestsLoanSteps();
+    }
+
+    @Step("Requesting a loan")
+    public void userRequestsLoanSteps() throws InterruptedException {
         String testCaseName = getCurrentTestName(getCurrentTestResult());
         Log.startTestCase(testCaseName);
         HomePageUserRegistration paraBankHomePageRegistration = new HomePageUserRegistration(driver);
@@ -73,8 +106,14 @@ public class ParaBankTests extends BaseTest {
         requestLoan.processLoanRequest();
         Log.endTestCase(testCaseName);
     }
-    @Test(priority = 6)
+
+    @Test(priority = 6, description = "Pay bills")
     public void userPayBillsTest() throws InterruptedException {
+        userPayBillsSteps();
+    }
+
+    @Step("Paying bills")
+    public void userPayBillsSteps() throws InterruptedException {
         String testCaseName = getCurrentTestName(getCurrentTestResult());
         Log.startTestCase(testCaseName);
         HomePageUserRegistration paraBankHomePageRegistration = new HomePageUserRegistration(driver);
@@ -89,8 +128,14 @@ public class ParaBankTests extends BaseTest {
         Assert.assertTrue(billPayment.validatePayee());
         Log.endTestCase(testCaseName);
     }
-    @Test(priority = 7)
+
+    @Test(priority = 7, description = "Find a transaction")
     public void userFindsTransactionTest() throws InterruptedException {
+        userFindsTransactionSteps();
+    }
+
+    @Step("Finding a transaction")
+    public void userFindsTransactionSteps() throws InterruptedException {
         String testCaseName = getCurrentTestName(getCurrentTestResult());
         Log.startTestCase(testCaseName);
         HomePageUserRegistration paraBankHomePageRegistration = new HomePageUserRegistration(driver);
@@ -107,4 +152,5 @@ public class ParaBankTests extends BaseTest {
         Log.endTestCase(testCaseName);
     }
 }
+
 //zmienić Thread sleep na wait > to nie wiem OCB nie moge zrobić
